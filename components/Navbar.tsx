475622b5c31ac03dcaaf95b0fdcc418b002caab0
aditya-yadav08 <aditya.yadav2708@gmail.com>
2024-09-07 22:48:54 +0530

    Clerk implementation

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,7 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import MobileNav from './MobileNav';
-// import { SignedIn, UserButton } from '@clerk/nextjs';
+import { SignedIn, UserButton } from '@clerk/nextjs';
 
 
 const Navbar = () => {
@@ -20,9 +20,10 @@ const Navbar = () => {
         </p>
       </Link>
       <div className="flex-between gap-5">
-        {/* <SignedIn>
-          <UserButton afterSignOutUrl="/sign-in" />
-        </SignedIn>*/}
+        <SignedIn>
+          <UserButton />
+          afterSignOutUrl="/sign-in"
+        </SignedIn>
 
         <MobileNav /> 
       </div>
